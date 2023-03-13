5d48102f47539253b2f2155be36bcb4f1a6343d8
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-03-13 14:18:13 +0100

    adapted object detection

--- object_detection.py
+++ object_detection.py
@@ -5,7 +5,7 @@ from PIL import Image
 from transformers import DetrForObjectDetection, DetrImageProcessor
 
 file_name = sys.argv[1]
-threshold = float(sys.argv[2])
+number_of_boxes = int(sys.argv[2])
 
 image = Image.open(file_name).convert('RGB')
 
@@ -18,13 +18,14 @@ outputs = model(**inputs)
 # convert outputs (bounding boxes and class logits) to COCO API
 # let's only keep detections with score > 0.9
 target_sizes = torch.tensor([image.size[::-1]])
-processed = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=threshold)
-results = processed[0]
-print(f'Threshold: {threshold}')
-print(f'number of boxes: {len(results["scores"])}')
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    if score < threshold:
-        continue
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0)[0]
+
+print(f'number of boxes: {number_of_boxes}')
+
+sorted_zipped = sorted(zip(results["scores"], results["labels"], results["boxes"]), key=lambda x:x[0], reverse=True)
+for index, (score, label, box) in enumerate(sorted_zipped):
+    if index == number_of_boxes:
+        break
     box = [round(i, 2) for i in box.tolist()]
     print(
             f"Detected {model.config.id2label[label.item()]} with confidence "
