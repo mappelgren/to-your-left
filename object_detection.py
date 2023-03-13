cdda9357dc371c6d24fe149ed4b9f7835a28ede2
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-03-13 14:05:21 +0100

    adapted object detection

--- object_detection.py
+++ object_detection.py
@@ -15,8 +15,9 @@ outputs = model(**inputs)
 # convert outputs (bounding boxes and class logits) to COCO API
 # let's only keep detections with score > 0.9
 target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
-
+processed = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=float(sys.argv[2]))
+print(len(processed))
+results = processed[0]
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     box = [round(i, 2) for i in box.tolist()]
     print(
