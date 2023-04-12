9dbdd7233855626a54aadfd5508595b539ae4009
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-04-12 19:00:35 +0200

    fix model

--- source/preexperiments/classification_models.py
+++ source/preexperiments/classification_models.py
@@ -83,7 +83,7 @@ class ResnetAttentionAttributeClassifier(Module):
         image, color_tensor, shape_tensor, size_tensor = data
         resnet = self.resnet(image)
         pooled = self.adaptive_pool(resnet)
-
+        print(pooled.shape)
         concatenated = torch.cat((pooled, color_tensor, shape_tensor, size_tensor))
         classified = self.classifier(concatenated)
 
