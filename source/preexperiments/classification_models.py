bad78d6974d363fdb1b06c3c2d5130a34919571c
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-04-12 18:23:45 +0200

    pixel model with attributes

--- source/preexperiments/classification_models.py
+++ source/preexperiments/classification_models.py
@@ -12,7 +12,7 @@ class ResnetBoundingBoxClassifier(Module):
         self.resnet.eval()
 
         self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
-        
+
         self.classifier = nn.Sequential(
             nn.Linear(20480, 4096),
             # nn.ReLU(),
@@ -45,7 +45,7 @@ class ResnetAttentionClassifier(Module):
         self.resnet.eval()
 
         self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7))
-        
+
         self.classifier = nn.Sequential(
             nn.Linear(100352, 1024),
             nn.ReLU(),
@@ -59,3 +59,31 @@ class ResnetAttentionClassifier(Module):
         classified = self.classifier(torch.flatten(pooled, start_dim=1))
 
         return classified
+
+class ResnetAttentionAttributeClassifier(Module):
+    def __init__(self, number_colors=8, number_shapes=3, number_size=2) -> None:
+        super().__init__()
+        resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.resnet = nn.Sequential(*list(resnet.children())[:-2])
+        self.resnet.eval()
+
+        # out 2048
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2048 + number_colors + number_shapes + number_size, 2),
+            # nn.ReLU(),
+            # nn.Linear(1024, 2),
+        )
+
+    def forward(self, data):
+        image, color_tensor, shape_tensor, size_tensor = data
+        resnet = self.resnet(image)
+        pooled = self.adaptive_pool(resnet)
+
+        concatenated = torch.cat(pooled, color_tensor, shape_tensor, size_tensor)
+        classified = self.classifier(concatenated)
+
+        return classified
+    
--- source/preexperiments/data_readers.py
+++ source/preexperiments/data_readers.py
@@ -1,11 +1,31 @@
 import json
 import random
+from enum import Enum
 
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.models import ResNet50_Weights
 
+
+class Shape(Enum):
+    CUBE = 0
+    SPHERE = 1
+    CYLINDER = 2
+
+class Color(Enum):
+    GRAY = 0
+    RED = 1
+    BLUE = 2
+    GREEN = 3
+    BROWN = 4
+    PURPLE = 5
+    CYAN = 6
+    YELLOW = 7
+
+class Size(Enum):
+    SMALL = 0
+    LARGE = 1
 
 class ClassifierDataset(Dataset):
     def __init__(self, scenes_json_file, image_path, max_number_samples) -> None:
@@ -81,6 +101,43 @@ class AttentionDataset(Dataset):
 
             self.samples.append((preprocess(image), torch.tensor([target_x, target_y])))
 
+    def __getitem__(self, index):
+        return self.samples[index]
+
+    def __len__(self) -> int:
+        return len(self.samples)
+    
+class AttentionAttributeDataset(Dataset):
+    def __init__(self, scenes_json_file, image_path, max_number_samples) -> None:
+        super().__init__()
+
+        preprocess = ResNet50_Weights.DEFAULT.transforms()
+        self.samples = []
+
+        with open(scenes_json_file, 'r', encoding='utf-8') as f:
+            scenes_metadata = json.load(f)
+        for index, scene in enumerate(scenes_metadata['scenes']):
+            if index == max_number_samples:
+                break
+
+            image = Image.open(image_path + scene['image_filename']).convert('RGB')
+
+            target_object = scene['groups']['target'][0]
+            target_x, target_y, _ = scene['objects'][target_object]['pixel_coords']
+
+            color_tensor = self._one_hot_encode(Color, scene['objects'][target_object]['color'])
+            shape_tensor = self._one_hot_encode(Shape, scene['objects'][target_object]['shape'])
+            size_tensor = self._one_hot_encode(Size, scene['objects'][target_object]['size'])
+
+            self.samples.append(((preprocess(image), color_tensor, shape_tensor, size_tensor),
+                                 torch.tensor([target_x, target_y])))
+
+    def _one_hot_encode(self, attribute: Enum, value: str):
+        tensor = torch.zeros(len(attribute))
+        tensor[attribute[value.upper()].value] = 1
+
+        return tensor
+    
     def __getitem__(self, index):
         return self.samples[index]
 
