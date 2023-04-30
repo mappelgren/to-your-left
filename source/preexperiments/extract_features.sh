3194285825016e74492ec21fbe17bfbe1dd90e24
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-05-01 00:14:20 +0200

    extract features script

--- source/preexperiments/extract_features.sh
+++ source/preexperiments/extract_features.sh
@@ -4,8 +4,8 @@ out_dir=$2
 batch_size=$3
 device=$4
 
-python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_1_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 1 --device $device --batch_size $batch_size
-python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_2_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 2 --device $device --batch_size $batch_size
+#python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_1_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 1 --device $device --batch_size $batch_size
+#python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_2_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 2 --device $device --batch_size $batch_size
 python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_3_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 3 --device $device --batch_size $batch_size
 python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_4_no-avgpool_no-fc.h5 --feature_extractor ResNet --no-avgpool --no-fc --num_blocks 4 --device $device --batch_size $batch_size
 python source/preexperiments/feature_extractors.py --image_dir $image_dir --out_file "$out_dir"resnet_4_avgpool_no-fc.h5 --feature_extractor ResNet --avgpool --no-fc --num_blocks 4 --device $device --batch_size $batch_size
