29ae929dae91fb9f2ed9ba411e4cad274b5c01b5
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-10-17 18:31:04 +0200

    experiments

--- source/mlt/preexperiments/run_experiments.py
+++ source/mlt/preexperiments/run_experiments.py
@@ -3,23 +3,21 @@ import subprocess
 
 options = [
     "--dataset_base_dir=/home/dominik/Development/",
-    "--epochs=40",
+    "--epochs=30",
     "--lr=0.0002",
     "--feature_file=resnet_3_no-avgpool_no-fc.h5",
     "--max_samples=10000",
     "--device=cuda",
-    "--model=masked_caption_generator",
+    "--model=attribute_location_coordinate_predictor",
 ]
 
 variables = {
-    "--dataset": ["dale-2", "dale-5", "colour"],
-    "--embedding_dim": [10, 15, 30],
-    "--decoder_out_dim": [100, 500, 1000],
+    "--dataset": ["single", "dale-2", "dale-5", "colour"],
+    "--image_embedding_dimension": [100, 500, 1000],
+    "--coordinate_classifier_dimension": [512, 1024, 2048],
 }
 
 for index, combination in enumerate(itertools.product(*variables.values())):
-    if index == 0:
-        continue
     save_appendix = "_".join(str(i) for i in combination[1:])
     subprocess.run(
         [
@@ -31,5 +29,6 @@ for index, combination in enumerate(itertools.product(*variables.values())):
                 for option, value in zip(variables.keys(), combination)
             ],
             f"--save_appendix={save_appendix}",
-        ]
+        ],
+        check=False,
     )
