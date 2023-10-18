f40cf97b598b716b563b913bfae98abefaa2f5d6
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-10-18 19:57:15 +0200

    experiments

--- source/mlt/preexperiments/run_experiments.py
+++ source/mlt/preexperiments/run_experiments.py
@@ -8,7 +8,7 @@ options = [
     "--feature_file=resnet_3_no-avgpool_no-fc.h5",
     "--max_samples=10000",
     "--device=cuda",
-    "--model=masked_coordinate_predictor",
+    "--model=all_masked_coordinate_predictor",
     "--out=/scratch/guskunkdo/out/",
 ]
 
