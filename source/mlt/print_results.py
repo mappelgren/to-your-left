773fe8a4a607bd3188c34325b1785d3b1cf04bb9
Dominik Künkele <dominik.kuenkele@outlook.com>
2023-11-01 14:58:47 +0100

    evaluation

--- source/mlt/print_results.py
+++ source/mlt/print_results.py
@@ -2,7 +2,6 @@ import argparse
 import glob
 import json
 import os
-import pprint
 import re
 from dataclasses import dataclass
 
@@ -10,24 +9,31 @@ from dataclasses import dataclass
 @dataclass
 class Experiment:
     folder_name: str
-    variable_string: str
     dataset: str
-    variables: dict
+    variables: tuple
     runs: list[dict]
-    last_run: dict
+
+    @property
+    def last_run(self):
+        return self.runs[-1]
 
 
-def sort_by_variable(runs):
-    return dict(
-        sorted(
-            runs.items(),
-            key=lambda x: x[1][1],
-            reverse=False,
+def calculate_f1(run):
+    if "word_by_word_precision" in run.keys() and "word_by_word_recall" in run.keys():
+        run["word_by_word_f1"] = (
+            2
+            * (run["word_by_word_precision"] * run["word_by_word_recall"])
+            / (run["word_by_word_precision"] + run["word_by_word_recall"])
         )
-    )
+
+    return run
 
 
-sorts = {"metric": sort_by_metric, "variable": sort_by_variable}
+def calculate_metrics(run):
+    run = calculate_f1(run)
+
+    return run
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -37,24 +43,22 @@ if __name__ == "__main__":
         type=str,
         help="path to the results folders (can contain wildcards)",
     )
-
     parser.add_argument(
         "--variables",
         nargs="+",
         help="variables in the same order as in the folder name",
     )
-
     parser.add_argument(
         "--variable_order",
         nargs="+",
         type=int,
-        help="variables in the same order as in the folder name",
+        help="order of the chosen variables",
     )
-
     parser.add_argument(
-        "--sort",
-        choices=sorts.keys(),
-        help="sorting of the results",
+        "--datasets",
+        nargs="+",
+        type=str,
+        help="datasets in order in which they should appear in the table",
     )
 
     args = parser.parse_args()
@@ -78,9 +82,7 @@ if __name__ == "__main__":
             variables = [int(var) for var in match.group(2).split("_") if var != ""]
             if len(variables) != len(args.variables):
                 raise ValueError(f"Number of Variables don't match: {folder_name}")
-            ordered_variables = [variables[o] for o in args.variable_order]
-
-        variable_string = "_".join(map(str, ordered_variables))
+            ordered_variables = tuple(variables[o] for o in args.variable_order)
 
         with open(os.path.join(folder, "log.txt"), "r", encoding="utf-8") as f:
             runs = []
@@ -94,42 +96,36 @@ if __name__ == "__main__":
                     for k, v in epoch.items():
                         if isinstance(v, dict):
                             for sub_k, sub_v in v.items():
-                                flattened[f"{k}_{sub_k}"] = sub_v
+                                flattened[f"{k}_{sub_k}"] = float(sub_v)
                         else:
-                            flattened[k] = v
+                            flattened[k] = float(v)
 
-                    runs.append(flattened)
+                    extended = calculate_metrics(flattened)
+
+                    runs.append(extended)
 
             if len(runs) != 0:
                 experiments.append(
                     Experiment(
                         folder_name=folder_name,
-                        variable_string=variable_string,
                         dataset=dataset,
-                        variables=variables,
+                        variables=ordered_variables,
                         runs=runs,
-                        last_run=runs[-1],
                     )
                 )
 
-    all_metrics = set(experiment.last_run.keys() for experiment in experiments)
+    all_metrics = set(
+        metric for experiment in experiments for metric in experiment.last_run.keys()
+    )
     sorted_metrics = list(sorted(all_metrics))
     print(list(enumerate(sorted_metrics)))
 
-    choice = input("choose important metrics (separate by comma):")
+    choice = input("choose important metrics (separated by comma):")
     chosen_metrics = [sorted_metrics[int(c)] for c in choice.split(",")]
 
-    sorted_experiments = list(
-        sorted(
-            experiments,
-            key=lambda experiment: experiment.variables,
-            reverse=False,
-        )
-    )
-
-    all_datasets = set(experiment.dataset for experiment in experiments)
-    all_variables = set(experiment.variables for experiment in experiments)
-    all_variations = set(experiment.variable_string for experiment in experiments)
+    all_datasets = args.datasets
+    all_variables = set(args.variables[i] for i in args.variable_order)
+    sorted_variations = sorted(set(experiment.variables for experiment in experiments))
 
     number_datasets = len(all_datasets)
     number_variables = len(all_variables)
@@ -149,38 +145,46 @@ if __name__ == "__main__":
     )
 
     metric_header_per_dataset = " & ".join(
-        [rf"\textbf{{{metric}}}" for metric in chosen_metrics]
+        [rf"\textbf{{{metric.replace('_', '-')}}}" for metric in chosen_metrics]
     )
     metric_header = f" & {metric_header_per_dataset}" * number_datasets
 
     result_lines = []
-    for run_name, _, variables in runs:
-        variable_string = " & ".join([f"{{{var}}}" for var in variables])
-        pprint.pprint(sorted_runs.values())
+    for variation in sorted_variations:
+        variation_experiments = [
+            experiment
+            for experiment in experiments
+            if experiment.variables == variation
+        ]
+
+        variable_string = "        " + " & ".join(
+            [f"{{{variable}}}" for variable in variation]
+        )
+
         result_string = " & ".join(
             [
-                f"{{{dataset[run_name][0][metric]}}}"
-                for dataset in sorted_runs.values()
+                rf"{{{str(round(experiment.last_run[metric] * 100, 2)).rstrip('0.').replace('.', ',')}\%}}"
+                for dataset in all_datasets
+                for experiment in variation_experiments
                 for metric in chosen_metrics
+                if experiment.dataset == dataset
             ]
         )
         result_lines.append(rf"{variable_string} & {result_string} \\")
 
     results_block = "\n".join(result_lines)
 
-    latex_stub = rf"""
-\begin{{table}}[ht]
+    latex_stub = rf"""\begin{{table}}[ht]
     \centering
     \begin{{tabular}}{{{'c'*number_variables}{f"|{'c'*number_metrics}"*number_datasets}}}
         \toprule
         {' &' *number_variables}{dataset_header} \\  {cmidrule_header}
-        {' & '.join([args.variables[o].replace('_', '-') for o in args.variable_order])}     {metric_header}    \\\midrule
-        {results_block}
+        {' & '.join([args.variables[o].replace('_', '-') for o in args.variable_order])} {metric_header} \\\midrule
+{results_block}
         \bottomrule
     \end{{tabular}}
     \caption{{test}}
     \label{{test}}
-\end{{table}}
-"""
+\end{{table}}"""
 
     print(latex_stub)
